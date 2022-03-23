a18d6763135c61873134a0f6461d55ee9c645847
DervisevicE <edervisevi2@etf.unsa.ba>
2022-03-23 13:41:20 +0100

    zadatak1

--- app/src/main/java/com/example/lv1/MainActivity.kt
+++ app/src/main/java/com/example/lv1/MainActivity.kt
@@ -2,24 +2,39 @@ package com.example.lv1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var listView: ListView
+    private lateinit var button: Button
+    private lateinit var editText: EditText
+
+    private val listaVrijednosti = arrayListOf<String>()
+    private lateinit var adapter: MyArrayAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.btn1)
+
+        button = findViewById(R.id.button1)
+        editText = findViewById(R.id.editText1)
+        listView = findViewById(R.id.listView1)
+
+        adapter = MyArrayAdapter(this, android.R.layout.simple_list_item_1, listaVrijednosti)
+        listView.adapter = adapter
+
         button.setOnClickListener{
-            showMessage()
+            addToList()
         }
     }
 
-    private fun showMessage() {
-        val editText = findViewById<EditText>(R.id.editText1)
-        val textView = findViewById<TextView>(R.id.textView1)
-        val message = editText.text.toString()
-        textView.text = message
+    private fun addToList() {
+        listaVrijednosti.add(0,editText.text.toString())
+        adapter.notifyDataSetChanged()
+        editText.setText("")
     }
 }
